d99bb28803ea8f43e0ad5fa20e658d1a5fd926e3
killshotxd <hassanansari211@gmail.com>
2023-05-02 14:11:55 +0530

    User Card Done

--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -4,7 +4,7 @@ const Home = () => {
   return (
     <>
       <div className="py-4 mx-auto container px-10">
-        <div className="grid grid-cols-3 max-sm:grid-cols-1 max-md:justify-center max-md:grid-cols-2 lg:grid-cols-3 gap-4">
+        <div className="grid grid-cols-3 max-sm:grid-cols-1 max-md:justify-center max-md:grid-cols-2 lg:grid-cols-3 gap-8">
           <div className="sideBar">
             <UserHomeCard />
           </div>
--- src/components/pages/Home/UserHomeCard.jsx
+++ src/components/pages/Home/UserHomeCard.jsx
@@ -1,5 +1,90 @@
+import { UserAuth } from "../../context/AuthContext";
+import { GrEdit, GrUserSettings } from "react-icons/gr";
+import { GoLocation } from "react-icons/go";
+import { MdWork } from "react-icons/md";
 const UserHomeCard = () => {
-  return <>UserHomeCard</>;
+  const { currentUser } = UserAuth();
+  return (
+    <>
+      <div className="card w-full bg-base-100 shadow-md">
+        <div className="card-body">
+          {/* NAME,Friends */}
+          <div className="flex justify-between items-center">
+            <div className="flex items-center gap-4">
+              <div className="avatar online">
+                <div className="w-10 rounded-full">
+                  <img src={currentUser.photoURL} />
+                </div>
+              </div>
+              <div className="flex flex-col">
+                <span className="font-bold">{currentUser.displayName}</span>
+                <span className="text-sm">0 Friends</span>
+              </div>
+            </div>
+
+            <span>
+              <GrUserSettings />
+            </span>
+          </div>
+          {/* NAME,Friends */}
+          <hr />
+          {/* LOCATION */}
+          <div>
+            <small className="flex items-center gap-2">
+              <GoLocation /> Uttar Pradesh
+            </small>
+            <small className="flex items-center gap-2">
+              <MdWork /> Frontend Developer
+            </small>
+          </div>
+          {/* LOCATION */}
+          <hr />
+
+          {/* PROFILE VIEWS/IMPRESSIONS */}
+          <small>
+            Profile Views in last 7 days :{" "}
+            <span className="font-semibold">21</span>{" "}
+          </small>
+          <small>
+            Impressions in last 7 days :{" "}
+            <span className="font-semibold">1021</span>
+          </small>
+          {/* PROFILE VIEWS/IMPRESSIONS */}
+          <hr />
+          {/* SOCIAL LINKS */}
+          <div className="flex items-center justify-between">
+            <div className="flex items-center gap-2">
+              <div>logo</div>
+              <div className="flex flex-col">
+                <small>name</small>
+                <small>name</small>
+              </div>
+            </div>
+
+            <small>
+              <GrEdit />
+            </small>
+          </div>
+          {/* SOCIAL LINKS */}
+          {/* SOCIAL LINKS */}
+          <div className="flex items-center justify-between">
+            <div className="flex items-center gap-2">
+              <div>logo</div>
+              <div className="flex flex-col">
+                <small>name</small>
+                <small>name</small>
+              </div>
+            </div>
+
+            <small>
+              <GrEdit />
+            </small>
+          </div>
+          {/* SOCIAL LINKS */}
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default UserHomeCard;

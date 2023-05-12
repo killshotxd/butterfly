ce1f68f7f7bd8edeb4e430451bcdeba6ce768256
killshotxd <hassanansari211@gmail.com>
2023-05-12 11:21:33 +0530

    Fixed responsiveness
    Infinite Scroll Feed Implemented:
    Infinite Message Box

--- src/components/pages/MessageBox/Message.jsx
+++ src/components/pages/MessageBox/Message.jsx
@@ -7,7 +7,7 @@ const Message = () => {
 
   return (
     <>
-      <div className="py-4 mx-auto bg-base-200 min-h-screen container px-10">
+      <div className="py-4 mx-auto bg-base-200 msgContainer container px-10">
         <div className="grid grid-cols-2 max-sm:grid-cols-1 max-md:justify-center max-md:grid-cols-2 lg:grid-cols-2 gap-8">
           <div className="flex flex-col gap-4">
             <UserHomeCard state={state} />
